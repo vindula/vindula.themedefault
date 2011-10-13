64a4e114586b210b5ff61ee2e0e81b33b3ef3fb9
Fabio <fabiorizzo@liberiun.com>
2011-10-13 15:33:23 -0300

    Another fixes

---
+++ vindula/myvindula/myvindula.py
@@ -0,0 +1,148 @@
+from Acquisition import aq_inner
+from five import grok
+from plone.app.layout.navigation.interfaces import INavigationRoot
+from Products.CMFCore.interfaces import ISiteRoot
+
+from plone.dexterity.utils import createContentInContainer
+from plone.namedfile.field import NamedImage
+from Products.CMFCore.utils import getToolByName
+#from vindula.myvindula.user import IFuncDetails
+from plone.directives import form
+from vindula.myvindula import MessageFactory as _
+from z3c.form import button
+from zope import schema
+from Products.statusmessages.interfaces import IStatusMessage
+from vindula.myvindula.user import IFuncDetails
+
+
+
+
+
+
+class MyVindulaView(grok.View):
+    grok.context(INavigationRoot)
+    grok.require('zope2.View')
+    grok.name('myvindula')
+    
+    def checkHomeFolder(self):
+        """ Check if exist homeFolder """
+        
+        homefolder = self.context.portal_membership.getHomeFolder()
+        if homefolder:
+            return True
+        else:
+            return False
+        
+
+    def update(self):
+        """ Receive itself from request and do some actions """
+        
+        form = self.request.form
+        submitted = form.get('form.submitted', False)
+        
+        if submitted:
+            
+            homefolder = self.context.portal_membership.getHomeFolder()
+            howareu = form.get('howareu')
+            item = createContentInContainer(homefolder, "vindula.myvindula.howareu", title=howareu)
+
+            
+
+class MyVindulaPanelView(grok.View):
+    grok.context(INavigationRoot)
+    grok.require('zope2.View')
+    grok.name('myvindulapanel')
+    
+    
+    def _checkPermission(self, permission, context):
+        mt = getToolByName(context, 'portal_membership')
+        return mt.checkPermission(permission, context)
+    
+    
+    def getPersonalInfoLink(self):
+        """ Get the link for vindula home """
+        
+        context = aq_inner(self.context)        
+        template = None
+        if self._checkPermission('Set own properties', context):
+            template = '@@myvindulapanel?section=myvindula'
+        return template
+    
+    
+
+    def getPersonalPrefsLink(self):
+        """ Get the link for user preferences """
+        
+        context = aq_inner(self.context)        
+        template = None
+        if self._checkPermission('Set own properties', context):
+            template = '@@myvindulapanel?section=myvindulaprefs'
+        return template
+
+
+
+
+    def getMacroLink(self, page):
+        #import pdb; pdb.set_trace()
+
+        path = 'context/@@%s/macros/page' % page
+        return path
+
+
+
+    
+
+
+
+
+class MyVindulaPrefsView(form.SchemaForm):
+    grok.context(ISiteRoot)
+    grok.require('zope2.View')
+    grok.name('myvindulaprefs')
+    
+    schema = IFuncDetails
+    ignoreContext = True
+    
+    
+    label = _(u"Personal Informations")
+    description = _(u"Change your available information below.")   
+
+    
+    def update(self):
+        # disable Plone's editable border
+        self.request.set('disable_border', True)
+        
+        # call the base class version - this is very important!
+        super(MyVindulaPrefsView, self).update()
+    
+    @button.buttonAndHandler(_(u'Order'))
+    def handleApply(self, action):
+        data, errors = self.extractData()
+        if errors:
+            self.status = self.formErrorsMessage
+            return
+        
+        
+        # Put the threatment to database
+
+        
+        # Redirect back to the front page with a status message
+
+        IStatusMessage(self.request).addStatusMessage(
+                _(u"Thank you for your order. We will contact you shortly"), 
+                "info"
+            )
+        
+        contextURL = self.context.absolute_url()
+        self.request.response.redirect(contextURL)
+        
+    @button.buttonAndHandler(_(u"Cancel"))
+    def handleCancel(self, action):
+        """User cancelled. Redirect back to the front page.
+        """
+        contextURL = self.context.absolute_url()
+        self.request.response.redirect(contextURL)
+        
+        
+
+
---
+++ vindula/myvindula/user.py
@@ -0,0 +1,187 @@
+from five import grok
+from plone.directives import form
+
+from zope import schema
+from z3c.form import button
+from plone.namedfile.field import NamedImage
+
+from vindula.myvindula import MessageFactory as _
+
+class IFuncDetails(form.Schema):
+    
+    name = schema.TextLine(
+    title=_(u"name"),
+    description=_(u"Digite o nome deste funcionario"),
+    required=False,
+    )
+    phone_number = schema.TextLine(
+    title=_(u"Telefone"),
+    description=_(u"Digite o Telefone do funcionario"),
+    required=False,
+    )
+    email = schema.TextLine(
+    title=_(u"E mail"),
+    description=_(u"Digite o E-mail do funcionario"),
+    required=False,
+    )
+    employee_id = schema.TextLine(
+    title=_(u"Employee ID"),
+    description=_(u"Digite o ID do funcionario"),
+    required=False,
+    )
+    data_nascimento = schema.TextLine(
+    title=_(u"Data de Nascimento"),
+    description=_(u"Digite a data de nascimneto do funcionario"),
+    required=False,
+    )
+    empresa = schema.TextLine(
+    title=_(u"Empresa"),
+    description=_(u"Digite o nome da empresa do funcionario"),
+    required=False,
+    )                
+    cargo = schema.TextLine(
+    title=_(u"Cargo"),
+    description=_(u"Digite o cargo do funcionario"),
+    required=False,
+    )    
+    data_admissao = schema.TextLine(
+    title=_(u"Data de Admissao"),
+    description=_(u"Digite a data de admissao do funcionario"),
+    required=False,
+    )    
+    centro_custo = schema.TextLine(
+    title=_(u"Centro de Custo"),
+    description=_(u"Digite o centro de custo do funcionario"),
+    required=False,
+    )    
+    job_role = schema.TextLine(
+    title=_(u"Job Role"),
+    description=_(u"Digite o 'Job Role' do funcionario"),
+    required=False,
+    )        
+    organisational_unit = schema.TextLine(
+    title=_(u"Organisational Unit"),
+    description=_(u"Digite o 'Organisational Unit' do funcionario"),
+    required=False,
+    )
+    reports_to = schema.TextLine(
+    title=_(u"Reports to"),
+    description=_(u"Digite o 'Reports to' do funcionario"),
+    required=False,
+    )    
+    location = schema.TextLine(
+    title=_(u"Localizacao"),
+    description=_(u"Digite a localizacao do funcionario"),
+    required=False,
+    )    
+    postal_address = schema.TextLine(
+    title=_(u"Endereco Postal"),
+    description=_(u"Digite o endereco do funcionario"),
+    required=False,
+    )    
+    special_roles = schema.TextLine(
+    title=_(u"Special Roles"),
+    description=_(u"Digite o 'Special Roles' do funcionario"),
+    required=False,
+    )    
+    photograph = NamedImage(
+    title=_(u"Foto"),
+    description=_(u"Coloque a foto do funcionario"),
+    required=False,
+    )    
+    nickname = schema.TextLine(
+    title=_(u"NickName"),
+    description=_(u"Digite o 'nickname' do funcionario"),
+    required=False,
+    )        
+    pronunciation_name = schema.TextLine(
+    title=_(u"Como pronuncia seu nome"),
+    description=_(u"Como pronuncia o  nome do funcionario"),
+    required=False,
+    )        
+    committess = schema.TextLine(
+    title=_(u"Commitess"),
+    description=_(u"Digite o 'Commitess' do funcionario"),
+    required=False,
+    )        
+    projetcs = schema.TextLine(
+        title=_(u"Projetos"),
+        description=_(u"Digite o Projeto do funcionario"),
+        required=False,
+    )        
+    personal_information = schema.TextLine(
+        title=_(u"Informacoes pessoais"),
+        description=_(u"Digite as informaoes pessoais do funcionario"),
+        required=False,
+    )        
+    skills_expertise = schema.TextLine(
+        title=_(u"Skills Expertise"),
+        description=_(u"Digite o 'Skills Expertise' do funcionario"),
+        required=False,
+    )        
+    license_plate_numbers = schema.TextLine(
+        title=_(u"License Plate Numbers"),
+        description=_(u"Digite as 'License Plate Numbers' do funcionario"),
+        required=False,
+    )        
+    profit_centre = schema.TextLine(
+        title=_(u"Profit Centre"),
+        description=_(u"Digite o 'Profit Centre' do funcionario"),
+        required=False,
+    )        
+    languages = schema.TextLine(
+        title=_(u"Languages"),
+        description=_(u"Digite a 'Languages' do funcionario"),
+        required=False,
+    )        
+    availability = schema.TextLine(
+        title=_(u"Avaliacao"),
+        description=_(u"Digite a avaliacao do funcionario"),
+        required=False,
+    )        
+    papers_published = schema.TextLine(
+        title=_(u"Artigo Publicados"),
+        description=_(u"Digite os artigo puclicados do funcionario"),
+        required=False,
+    )        
+    profit_centre = schema.TextLine(
+        title=_(u"Profit Centre"),
+        description=_(u"Digite o 'Profit Centre' do funcionario"),
+        required=False,
+    )        
+    teaching_research = schema.TextLine(
+        title=_(u"Teaching Research"),
+        description=_(u"Digite o 'Teaching Research' do funcionario"),
+        required=False,
+    )
+    delegations = schema.TextLine(
+        title=_(u"Delegacao"),
+        description=_(u"Digite a delegacao do funcionario"),
+        required=False,
+    ) 
+    resume = schema.TextLine(
+        title=_(u"Resumo"),
+        description=_(u"Digite o resumo do funcionario"),
+        required=False,
+    )          
+    blogs = schema.TextLine(
+        title=_(u"Blogs"),
+        description=_(u"Digite o blogs do funcionario"),
+        required=False,
+    )          
+    customised_message = schema.TextLine(
+        title=_(u"Menssagem Costumizada"),
+        description=_(u"Digite uma menssagem do funcionario"),
+        required=False,
+    )          
+    username = schema.TextLine(
+        title=_(u"Username"),
+        description=_(u"Digite uma username do funcionario"),
+        required=False,
+        )
+    departamentos_id = schema.Set(
+        title=_(u"departamentos_id"),
+        description=_(u"Selecione o departamento do funcionario"),
+            value_type=schema.Choice(values=[_(u'Margherita'), _(u'Pepperoni'), _(u'Hawaiian')]),
+        required=False,
+    ) 
