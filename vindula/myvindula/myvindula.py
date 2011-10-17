04e523668d1d77e20b24e0239567daf1223f0d0c
cesaraugusto <cesaraugusto@liberiun.com>
2011-10-17 15:13:50 -0200

    cadastro usuario

--- vindula/myvindula/myvindula.py
+++ vindula/myvindula/myvindula.py
@@ -12,13 +12,68 @@ from vindula.myvindula import MessageFactory as _
 from z3c.form import button
 from zope import schema
 from Products.statusmessages.interfaces import IStatusMessage
-from vindula.myvindula.user import IFuncDetails
+
+from plone.z3cform.crud import crud
+
+from vindula.myvindula.user import BaseStore, IFuncDetails, ModelsFuncDetails
+from storm.zope.interfaces import IZStorm
+from plone.i18n.normalizer.interfaces import IIDNormalizer
+from zope.component import getUtility
+
+class BaseFunc(BaseStore):
+    #default class for standard functions
+        
+    def uploadFile(self, path, file):
+        """ function used to upload the file to the Plone site, 
+           with the parameter where the file will be saved and the file will be saved
+        """
+        normalizer = getUtility(IIDNormalizer)
+        name_file = normalizer.normalize(file.filename)    #unicode(file.filename, 'utf-8')) #takes the name of the file
+
+        count = 0
+        while name_file in path.objectIds():
+            name_file = name_file + '-' + str(count)
+            count +=1
+        
+        #starts the upload process     
+        else:
+            try:
+                #if image ...    
+                if name_file.endswith('png') or \
+                    name_file.endswith('jpg') or \
+                        name_file.endswith('gif'):                
+
+                    createContentInContainer(path, "vindulaimage",
+                                             id= name_file,
+                                             title=name_file,
+                                             image=file)                
+                    
+                #if file ...
+                else:
+                    createContentInContainer(path, "File",
+                         id= name_file,
+                         title=name_file,
+                         image=file)                
+                    
+                #takes the url of the file to save the database  
+                content = getattr(path, name_file, None)
+                url = ''
+                if content is not None:
+                    url = content.absolute_url() 
+                
+                return unicode(url)
+                IStatusMessage(self.request).addStatusMessage(_(u"File upload successful."), 
+                                                              "info")
+                
+            except:
+                IStatusMessage(self.request).addStatusMessage(_(u'Error when trying to upload the file.'), 
+                                                              "erro")
+                access_denied = self.context.absolute_url() + '/portal_skins/plone_login/require_login'
+                self.request.response.redirect(access_denied)
 
 
-
-
-
-
+            
+            
 class MyVindulaView(grok.View):
     grok.context(INavigationRoot)
     grok.require('zope2.View')
@@ -67,8 +122,6 @@ class MyVindulaPanelView(grok.View):
         if self._checkPermission('Set own properties', context):
             template = '@@myvindulapanel?section=myvindula'
         return template
-    
-    
 
     def getPersonalPrefsLink(self):
         """ Get the link for user preferences """
@@ -79,19 +132,11 @@ class MyVindulaPanelView(grok.View):
             template = '@@myvindulapanel?section=myvindulaprefs'
         return template
 
-
-
-
     def getMacroLink(self, page):
         #import pdb; pdb.set_trace()
 
         path = 'context/@@%s/macros/page' % page
         return path
-
-
-
-    
-
 
 
 
@@ -106,25 +151,43 @@ class MyVindulaPrefsView(form.SchemaForm):
     
     label = _(u"Personal Informations")
     description = _(u"Change your available information below.")   
-
+            
     
     def update(self):
         # disable Plone's editable border
         self.request.set('disable_border', True)
+        return super(MyVindulaPrefsView, self).update()
         
-        # call the base class version - this is very important!
-        super(MyVindulaPrefsView, self).update()
     
     @button.buttonAndHandler(_(u'Order'))
     def handleApply(self, action):
+        self.store = getUtility(IZStorm).get('myvindula')
+        
         data, errors = self.extractData()
         if errors:
             self.status = self.formErrorsMessage
             return
         
-        
         # Put the threatment to database
-
+        
+        # Upload of Photograph
+        if data['photograph'] != None:
+            #convertidos['photograph'] = to_utf8(form['photograph'])
+            if data['photograph'].filename != '': 
+                path = self.context.portal_membership.getHomeFolder()
+                file = data['photograph']
+                data['photograph'] = BaseFunc().uploadFile(path,file)
+            else:
+                data['photograph'] = None    
+        
+        # adicionando...
+        data['username'] = unicode(self.context.portal_membership.getAuthenticatedMember().id, 'utf-8')
+        #data['Department_id'] = 1
+        
+        database = ModelsFuncDetails(**data)
+        
+        self.store.add(database)
+        self.store.flush()
         
         # Redirect back to the front page with a status message
 
@@ -143,6 +206,7 @@ class MyVindulaPrefsView(form.SchemaForm):
         contextURL = self.context.absolute_url()
         self.request.response.redirect(contextURL)
         
-        
 
 
+
+   
--- vindula/myvindula/user.py
+++ vindula/myvindula/user.py
@@ -5,104 +5,149 @@ from zope import schema
 from z3c.form import button
 from plone.namedfile.field import NamedImage
 
+from zope.schema.interfaces import IContextSourceBinder
+from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
+
 from vindula.myvindula import MessageFactory as _
+
+#Imports regarding the connection of the database 'strom'
+from storm.locals import *
+from zope.component import getUtility
+from storm.zope.interfaces import IZStorm
+from storm.locals import Store
+
+
+
+
+
+class BaseStore(object):
+   
+    def __init__(self, *args, **kwargs):
+        self.store = getUtility(IZStorm).get('myvindula')
+        
+        #Lazy initialization of the object
+        for attribute, value in kwargs.items():
+            if not hasattr(self, attribute):
+                
+                raise TypeError('unexpected argument %s' % attribute)
+            else:
+                setattr(self, attribute, value)        
+
 
 class IFuncDetails(form.Schema):
     
+    @grok.provider(IContextSourceBinder)
+    def choiceDepartament(context):
+        terms = []
+        departaments = ModelsDepartment().get_department()
+        if departaments != []:
+            for departament in departaments:
+                terms.append(SimpleVocabulary.createTerm(departament.id, str(departament.name)))
+                
+        return SimpleVocabulary(terms)    
+        
+
     name = schema.TextLine(
-    title=_(u"name"),
-    description=_(u"Digite o nome deste funcionario"),
-    required=False,
+        title=_(u"name"),
+        description=_(u"Digite o nome deste funcionario"),
+        required=False,
     )
     phone_number = schema.TextLine(
-    title=_(u"Telefone"),
-    description=_(u"Digite o Telefone do funcionario"),
-    required=False,
+        title=_(u"Telefone"),
+        description=_(u"Digite o Telefone do funcionario"),
+        required=False,
     )
     email = schema.TextLine(
-    title=_(u"E mail"),
-    description=_(u"Digite o E-mail do funcionario"),
-    required=False,
+        title=_(u"E mail"),
+        description=_(u"Digite o E-mail do funcionario"),
+        required=False,
     )
     employee_id = schema.TextLine(
-    title=_(u"Employee ID"),
-    description=_(u"Digite o ID do funcionario"),
-    required=False,
+        title=_(u"Employee ID"),
+        description=_(u"Digite o ID do funcionario"),
+        required=False,
     )
-    data_nascimento = schema.TextLine(
-    title=_(u"Data de Nascimento"),
-    description=_(u"Digite a data de nascimneto do funcionario"),
-    required=False,
+    date_birth = schema.Date(  #TextLine(
+        title=_(u"Data de Nascimento"),
+        description=_(u"Digite a data de nascimneto do funcionario"),
+        required=False,
     )
-    empresa = schema.TextLine(
-    title=_(u"Empresa"),
-    description=_(u"Digite o nome da empresa do funcionario"),
-    required=False,
+
+    registration = schema.TextLine(
+        title=_(u"Matricula"),
+        description=_(u"Digite a data de nascimneto do funcionario"),
+        required=False,
+    )    
+    
+    enterprise = schema.TextLine(
+        title=_(u"Empresa"),
+        description=_(u"Digite o nome da empresa do funcionario"),
+        required=False,
     )                
-    cargo = schema.TextLine(
-    title=_(u"Cargo"),
-    description=_(u"Digite o cargo do funcionario"),
-    required=False,
+    position = schema.TextLine(
+        title=_(u"Cargo"),
+        description=_(u"Digite o cargo do funcionario"),
+        required=False,
     )    
-    data_admissao = schema.TextLine(
-    title=_(u"Data de Admissao"),
-    description=_(u"Digite a data de admissao do funcionario"),
-    required=False,
+    admission_date = schema.Date( #TextLine(
+        title=_(u"Data de Admissao"),
+        description=_(u"Digite a data de admissao do funcionario"),
+        required=False,
     )    
-    centro_custo = schema.TextLine(
-    title=_(u"Centro de Custo"),
-    description=_(u"Digite o centro de custo do funcionario"),
-    required=False,
+    cost_center = schema.TextLine(
+        title=_(u"Centro de Custo"),
+        description=_(u"Digite o centro de custo do funcionario"),
+        required=False,
     )    
     job_role = schema.TextLine(
-    title=_(u"Job Role"),
-    description=_(u"Digite o 'Job Role' do funcionario"),
-    required=False,
+        title=_(u"Job Role"),
+        description=_(u"Digite o 'Job Role' do funcionario"),
+        required=False,
     )        
     organisational_unit = schema.TextLine(
-    title=_(u"Organisational Unit"),
-    description=_(u"Digite o 'Organisational Unit' do funcionario"),
-    required=False,
+        title=_(u"Organisational Unit"),
+        description=_(u"Digite o 'Organisational Unit' do funcionario"),
+        required=False,
     )
     reports_to = schema.TextLine(
-    title=_(u"Reports to"),
-    description=_(u"Digite o 'Reports to' do funcionario"),
-    required=False,
+        title=_(u"Reports to"),
+        description=_(u"Digite o 'Reports to' do funcionario"),
+        required=False,
     )    
     location = schema.TextLine(
-    title=_(u"Localizacao"),
-    description=_(u"Digite a localizacao do funcionario"),
-    required=False,
+        title=_(u"Localizacao"),
+        description=_(u"Digite a localizacao do funcionario"),
+        required=False,
     )    
     postal_address = schema.TextLine(
-    title=_(u"Endereco Postal"),
-    description=_(u"Digite o endereco do funcionario"),
-    required=False,
+        title=_(u"Endereco Postal"),
+        description=_(u"Digite o endereco do funcionario"),
+        required=False,
     )    
     special_roles = schema.TextLine(
-    title=_(u"Special Roles"),
-    description=_(u"Digite o 'Special Roles' do funcionario"),
-    required=False,
+        title=_(u"Special Roles"),
+        description=_(u"Digite o 'Special Roles' do funcionario"),
+        required=False,
     )    
     photograph = NamedImage(
-    title=_(u"Foto"),
-    description=_(u"Coloque a foto do funcionario"),
-    required=False,
+        title=_(u"Foto"),
+        description=_(u"Coloque a foto do funcionario"),
+        required=False,
     )    
     nickname = schema.TextLine(
-    title=_(u"NickName"),
-    description=_(u"Digite o 'nickname' do funcionario"),
-    required=False,
+        title=_(u"NickName"),
+        description=_(u"Digite o 'nickname' do funcionario"),
+        required=False,
     )        
     pronunciation_name = schema.TextLine(
-    title=_(u"Como pronuncia seu nome"),
-    description=_(u"Como pronuncia o  nome do funcionario"),
-    required=False,
+        title=_(u"Como pronuncia seu nome"),
+        description=_(u"Como pronuncia o  nome do funcionario"),
+        required=False,
     )        
     committess = schema.TextLine(
-    title=_(u"Commitess"),
-    description=_(u"Digite o 'Commitess' do funcionario"),
-    required=False,
+        title=_(u"Commitess"),
+        description=_(u"Digite o 'Commitess' do funcionario"),
+        required=False,
     )        
     projetcs = schema.TextLine(
         title=_(u"Projetos"),
@@ -174,14 +219,91 @@ class IFuncDetails(form.Schema):
         description=_(u"Digite uma menssagem do funcionario"),
         required=False,
     )          
-    username = schema.TextLine(
-        title=_(u"Username"),
-        description=_(u"Digite uma username do funcionario"),
-        required=False,
-        )
-    departamentos_id = schema.Set(
+    
+#    username = schema.TextLine(
+#        title=_(u"Username"),
+#        description=_(u"Digite uma username do funcionario"),
+#        required=False,
+#        )
+
+    Department_id = schema.Choice(
         title=_(u"departamentos_id"),
         description=_(u"Selecione o departamento do funcionario"),
-            value_type=schema.Choice(values=[_(u'Margherita'), _(u'Pepperoni'), _(u'Hawaiian')]),
+        source=choiceDepartament,
         required=False,
-    ) 
+    )
+    
+    
+class ModelsFuncDetails(Storm, BaseStore):    
+    __storm_table__ = 'FuncDetails'
+    
+    id = Int(primary=True)
+    name = Unicode()
+    phone_number = Unicode()
+    email = Unicode()
+    employee_id = Unicode()
+    username = Unicode()
+    date_birth = Date()
+    registration = Unicode()
+    enterprise = Unicode()
+    position = Unicode()
+    admission_date = Date()
+    cost_center = Unicode()
+    job_role = Unicode()
+    organisational_unit = Unicode()
+    reports_to = Unicode()
+    location = Unicode()
+    postal_address = Unicode()
+    special_roles = Unicode()
+    photograph = Unicode()
+    nickname = Unicode()
+    pronunciation_name = Unicode()
+    committess = Unicode()
+    projetcs = Unicode()
+    personal_information = Unicode()
+    skills_expertise = Unicode()
+    license_plate_numbers = Unicode()
+    profit_centre = Unicode()
+    languages = Unicode()
+    availability = Unicode()
+    papers_published = Unicode()
+    teaching_research =Unicode()
+    delegations = Unicode()
+    resume = Unicode()
+    blogs = Unicode()
+    customised_message = Unicode()
+    Department_id = Int()  
+  
+    #departamento = ReferenceSet(id, "Departamentos.departamentos_id")    
+    
+    def get_allFuncDetails(self):
+        data = self.store.find(ModelsFuncDetails)
+        if data.count() == 0:
+            return []
+        else:
+            return data
+    
+    def get_FuncDetails(self, user):
+        data = self.store.find(ModelsFuncDetails, ModelsFuncDetails.username==user).one()
+        if data:
+            return data
+        else:
+            return []
+
+   
+        
+        
+class ModelsDepartment(Storm, BaseStore):
+    __storm_table__ = 'Department'
+ 
+    id = Int(primary=True)
+    name = Unicode()
+    description = Unicode()
+    
+    #loads data into the combo "departamento_id"    
+    def get_department(self):
+        data = self.store.find(ModelsDepartment)
+        if data.count() == 0:
+            return []
+        else:
+            return data          
