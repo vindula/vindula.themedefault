9ad7cfba5e9c1143fcc3dfa3eebc382571ed4f52
cesaraugusto <cesaraugusto@liberiun.com>
2011-12-01 16:49:35 -0200

    aAjustes Myvindula lista de usuarios, commentarios e recados

---
+++ vindula/myvindula/static/comments-perfil.js
@@ -0,0 +1,62 @@
+$j = jQuery.noConflict();
+
+$j(document).ready(function(){		
+	
+	var user = $j('#user-igual').val();
+	if (user == 'True'){
+		$j('div#howareu').hide();
+		$j('div#recados').hide();
+		$j('div#perfil').show();
+	} else{
+		$j('div#perfil').hide();
+		$j('div#howareu').show();
+		$j('div#recados').hide();
+	};
+	
+	var variaveis = location.search.split("?");
+	var scraps = variaveis[1].lastIndexOf("scraps");
+	if (scraps != -1){
+		$j('div#howareu').hide();
+		$j('div#recados').show();
+		$j('div#perfil').hide();
+		
+		$j('li#howareu').removeClass('selected');
+		$j('li#scraps').addClass('selected');
+		$j('li#perfil').removeClass('selected');
+		
+	};
+	  
+	
+	$j('a#howareu').click(function(){
+		$j('div#howareu').show();
+		$j('div#recados').hide();
+		$j('div#perfil').hide();
+		
+		$j('li#howareu').addClass('selected');
+		$j('li#scraps').removeClass('selected');
+		$j('li#perfil').removeClass('selected');
+	});
+	
+	
+	$j('a#scraps').click(function(){
+		$j('div#howareu').hide();
+		$j('div#recados').show();
+		$j('div#perfil').hide();
+		
+		$j('li#howareu').removeClass('selected');
+		$j('li#scraps').addClass('selected');
+		$j('li#perfil').removeClass('selected');
+	});
+	
+	
+	$j('a#perfil').click(function(){
+		$j('div#howareu').hide();
+		$j('div#recados').hide();
+		$j('div#perfil').show();
+		
+		$j('li#howareu').removeClass('selected');
+		$j('li#scraps').removeClass('selected');
+		$j('li#perfil').addClass('selected');
+	});
+	
+});
