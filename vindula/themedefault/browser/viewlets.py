827500b605872923f34fa3ced284316f0aebec5a
Denis Raison <denisraison@liberiun.com>
2012-04-20 16:13:10 -0300

    Implementado o menu dropdown no portal
    O menu é usado de acordo com a flag no painel de controle

--- vindula/themedefault/browser/viewlets.py
+++ vindula/themedefault/browser/viewlets.py
@@ -5,6 +5,11 @@ from plone.app.layout.viewlets.interfaces import IPortalHeader, IAboveContent, I
 from Products.CMFCore.utils import getToolByName
 from zope.app.component.hooks import getSite
 
+
+from Products.CMFPlone import utils
+from Products.CMFPlone.browser.navigation import get_view_url
+from plone.app.layout.navigation.root import getNavigationRoot
+
 grok.context(Interface) 
 
 # Viewlet for portal logo top
@@ -105,16 +110,39 @@ class MenuViewlet(grok.Viewlet):
             return ['Folder', 'Link']
         else:
             return ['Folder', 'Link']
+        
+    def getUseDropDown(self):
+        result = False
+        portal = self.context.portal_url.getPortalObject()
+        if 'control-panel-objects' in portal.keys():
+            control = portal['control-panel-objects']
+            
+            if 'vindula_themeconfig' in control.keys():
+                thema = control['vindula_themeconfig']    
+                
+                try:itens = thema.ativa_menudropdown
+                except:itens = False
+                result = itens
+
+            return result
+        else:
+            return result
     
 
     def getMenu(self):
+        query={}
+        portal_properties = getToolByName(self.context, 'portal_properties')
+        navtree_properties = getattr(portal_properties, 'navtree_properties')
         portal = self.context.portal_url.getPortalObject()
-        types = self.getContentTypes()
+        query['types'] = self.getContentTypes()
+        if navtree_properties.getProperty('enable_wf_state_filtering', False):
+            query['review_state'] = navtree_properties.getProperty('wf_states_to_show', [])
                 
         urltool = getSite().portal_url
-        caminho = {'query': '/'.join(portal.getPhysicalPath()), 'depth': 1}
+        query['path'] = {'query': '/'.join(portal.getPhysicalPath()), 'depth': 1}
+        query['sort_on'] = 'getObjPositionInParent'
         ctool = getSite().portal_catalog
-        menus = ctool(portal_type=types, path=caminho, sort_on='getObjPositionInParent')        
+        menus = ctool(**query)      
         
         #menus = portal.objectValues(['ATFolder','ATLink','vindula.content.content.vindulacontentmacro'])
         if menus:
@@ -124,10 +152,14 @@ class MenuViewlet(grok.Viewlet):
                     L.append(obj.getObject())
             return L
         
-    def getSubMenu(self):
-        
+    def getSubMenu(self, drop=False):
+        query={}
+        portal_properties = getToolByName(self.context, 'portal_properties')
+        navtree_properties = getattr(portal_properties, 'navtree_properties')
         portal = self.context.portal_url.getPortalObject()
-        types = self.getContentTypes()
+        query['types'] = self.getContentTypes()
+        if navtree_properties.getProperty('enable_wf_state_filtering', False):
+            query['review_state'] = navtree_properties.getProperty('wf_states_to_show', [])
         
         if self.context.portal_type == 'HomePage':
             if self.context.getRef_itemMenu():
@@ -143,9 +175,10 @@ class MenuViewlet(grok.Viewlet):
                 context = context.aq_parent
                 
             urltool = getSite().portal_url
-            caminho = {'query': '/'.join(context.getPhysicalPath()), 'depth': 1}
+            query['path'] = {'query': '/'.join(context.getPhysicalPath()), 'depth': 1}
+            query['sort_on'] = 'getObjPositionInParent'
             ctool = getSite().portal_catalog
-            submenus = ctool(portal_type=types, path=caminho,sort_on='getObjPositionInParent')        
+            submenus = ctool(**query)
             
             #defaultView()
             #submenus = context.objectValues(('ATFolder','ATLink','vindula.content.content.vindulacontentmacro'))
@@ -155,6 +188,55 @@ class MenuViewlet(grok.Viewlet):
                     if self.checkObj(obj.getObject()):
                         L.append(obj.getObject())
                 return L
+            
+    def getSubMenuDrop(self, tab):
+        query={}
+        result=[]
+        portal_properties = getToolByName(self.context, 'portal_properties')
+        portal_catalog = getToolByName(self.context, 'portal_catalog')
+        navtree_properties = getattr(portal_properties, 'navtree_properties')
+        site_properties = getattr(portal_properties, 'site_properties')
+
+        rootPath = getNavigationRoot(self.context)
+        dpath='/'.join([rootPath,tab.id])
+        query['path'] = {'query' : dpath, 'depth' : 1}
+
+        query['portal_type'] = self.getContentTypes()
+
+        sortAttribute = navtree_properties.getProperty('sortAttribute', None)
+        if sortAttribute is not None:
+            query['sort_on'] = sortAttribute
+
+            sortOrder = navtree_properties.getProperty('sortOrder', None)
+            if sortOrder is not None:
+                query['sort_order'] = sortOrder
+
+        if navtree_properties.getProperty('enable_wf_state_filtering', False):
+            query['review_state'] = navtree_properties.getProperty('wf_states_to_show', [])
+
+        query['is_default_page'] = False
+
+        if site_properties.getProperty('disable_nonfolderish_sections', False):
+            query['is_folderish'] = True
+
+        # Get ids not to list and make a dict to make the search fast
+        idsNotToList = navtree_properties.getProperty('idsNotToList', ())
+        excludedIds = {}
+        for id in idsNotToList:
+            excludedIds[id]=1
+
+        rawresult = portal_catalog.searchResults(**query)
+
+        # now add the content to results
+        for item in rawresult:
+            if not (excludedIds.has_key(item.getId) or item.exclude_from_nav):
+                id, item_url = get_view_url(item)
+                data = {'name'       : utils.pretty_title_or_id(self.context, item),
+                        'id'         : item.getId,
+                        'url'        : item_url,
+                        'description': item.Description}
+                result.append(data)
+        return result
 
     def checkObj(self, obj):
         roles = self.context.portal_membership.getAuthenticatedMember().getRoles()
@@ -184,8 +266,12 @@ class MenuViewlet(grok.Viewlet):
             elif obj.absolute_url() in self.context.REQUEST.get('ACTUAL_URL'):
                 return 'selected'
         else:
-            if obj.absolute_url() in self.context.REQUEST.get('ACTUAL_URL'):
-                return 'selected'
+            try:
+                if obj.absolute_url() in self.context.REQUEST.get('ACTUAL_URL'):
+                    return 'selected'
+            except:
+                if obj['url'] in self.context.REQUEST.get('ACTUAL_URL'):
+                    return'selected'
     
     def isSelectedSubmenu(self, obj):
         obj_url = obj.absolute_url().split('/')
