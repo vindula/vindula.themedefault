c6fb4f374ce9df684eab7413ada4a4db9807806b
Cesar Augusto <cesaraugusto@liberiun.com>
2012-05-29 16:01:18 -0300

    Ajustes nos portlets ramais e aniversariantes

--- vindula/myvindula/portlets/ramais.py
+++ vindula/myvindula/portlets/ramais.py
@@ -167,7 +167,7 @@ class Renderer(base.Renderer):
                 if type(ramal) != unicode:
                     ramal = unicode(ramal, 'utf-8')
                     
-                result = ModelsFuncDetails().get_FuncBusca(title,'0',ramal,True)
+                result = ModelsFuncDetails().get_FuncBusca(title,'0',ramal,filtro_busca)
                 if result:
 
                     if departamento != '0' and self.data.filtro_departamento != 'departamentos':
