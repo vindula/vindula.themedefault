6ba82d332f01457aad75ca45ee6b6d6795904ef8
Denis Raison <denisraison@liberiun.com>
2012-07-03 17:55:25 -0300

    Portlet de configuracao do plone control panel igual o do vindula

--- vindula/themedefault/static/js/vindula_portlets.js
+++ vindula/themedefault/static/js/vindula_portlets.js
@@ -4,7 +4,7 @@ $j(document).ready(function(){
 	
 	/* Modificar portlet Navegação */ 
 	var html = '<dd class="portletFooter"></dd>';
-	$j('div.portletWrapper .portletNavigationTree').each(function(){
+	$j.find(['div.portletWrapper .portletNavigationTree', '#portlet-prefs']).each(function(){
 		$j(this).append(html);	
 	});
 	
