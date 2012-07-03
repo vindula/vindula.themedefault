dba391c5b5daa1aa63f161ec451447e3f24c3794
Denis Raison <denisraison@liberiun.com>
2012-07-03 18:14:17 -0300

    bug fix vindula portlets javascript

--- vindula/themedefault/static/js/vindula_portlets.js
+++ vindula/themedefault/static/js/vindula_portlets.js
@@ -4,7 +4,7 @@ $j(document).ready(function(){
 	
 	/* Modificar portlet Navegação */ 
 	var html = '<dd class="portletFooter"></dd>';
-	$j.find(['div.portletWrapper .portletNavigationTree, #portlet-prefs']).each(function(){
+	$j('div.portletWrapper .portletNavigationTree, #portlet-prefs').each(function(){
 		$j(this).append(html);	
 	});
 	
