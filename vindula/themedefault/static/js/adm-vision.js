251bf297e376279c4d3104d6e87f92182c04a7fb
Cesar Augusto <cesaraugusto@liberiun.com>
2014-03-18 17:41:39 -0300

    Adição de mecanismo de salvar em cookie

--- vindula/themedefault/static/js/adm-vision.js
+++ vindula/themedefault/static/js/adm-vision.js
@@ -9,7 +9,30 @@ $j(document).ready(function(){
         
 		$j(".adm-vision").each(function(){
 			$j(this).toggleClass('hide');
-			
-		});			
+		});
+		setcookie_vision();
+
 	});
+	setup_vision();
 });
+
+function setup_vision(){
+	var vision = $j.cookie("user_vision");
+	if (vision=='show'){
+        $j('.user-vision a span i').removeClass('vindula-icon-eye-4');
+        $j('.user-vision a span i').addClass('vindula-icon-eye');
+        
+		$j(".adm-vision").each(function(){
+			$j(this).removeClass('hide');
+		});
+	};
+};
+
+function setcookie_vision(){
+	var vision = $j.cookie("user_vision");
+	if (vision=='show'){
+		$j.cookie("user_vision",'hide');		
+	}else{
+		$j.cookie("user_vision",'show');		
+	};
+};
